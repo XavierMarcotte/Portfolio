fb3817efb88840a0c2ff918aae851310ea727228
XavierMarcotte <xaviermarcotte06@gmail.com>
2023-03-25 21:28:51 +0100

    ejs mise en place

--- app/router.js
+++ app/router.js
@@ -7,4 +7,18 @@ router.get('/', (req, res) => {
     res.render('accueil');
 });
 
+
+router.get('/projets', (req, res) => {
+    res.render('projets');
+});
+router.get('/competences', (req, res) => {
+    res.render('comp');
+});
+router.get('/apprentissage', (req, res) => {
+    res.render('learn');
+});
+router.get('/contact', (req, res) => {
+    res.render('contact');
+});
+
 export default router;
